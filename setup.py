6979c193bb8417981ef2536acd5ee62a9065c271
Aditi Vijay <aditivijay910@gmail.com>
2025-12-21 23:08:29 +0530

    project setup ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aditi Vijay",
+    author_email="aditivijay910@gmail.com",
+    packages=find_packages()
+)
